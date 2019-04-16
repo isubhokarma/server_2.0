516548e1683289f381cb5fa02ab1d79215a19cec
Subho Karmakar <isubhokarma@gmail.com>
2019-04-16 18:47:44 +0530

    Initial authentication setup -- Passportjs
    Authentication setup: passportjs
    Auth Strategy: passport-google-oauth20
    Google API: Oauth -- survey-dev
    Scopes: Google Profile, Email
    
    Calling the passport to use the Google Startegy to authenticate and scope: profile, email and revert back to callback url.
    
    Res: console.log(accessToken)

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/dev");
+
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send({ res: "Hello, World!" });
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: "/auth/google/callback"
+		},
+
+		//access authenticated and ready to create a new user or look for existing
+		accessToken => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	"/auth/google",
+	passport.authenticate("google", {
+		scope: ["profile", "email"]
+	})
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
