f38860f861c5e91f4c1762791757e0146ee1c816
Subho Karmakar <isubhokarma@gmail.com>
2019-04-27 01:44:00 +0530

    require: keys
    Don't forget to change the require target for keys

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cokieSession = require("cookie-session");
 const passport = require("passport");
-const keys = require("./config/dev");
+const keys = require("./config/keys");
 require("./models/user");
 require("./services/passport");
 
