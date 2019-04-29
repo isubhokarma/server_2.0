a2d9a74ccc6c536af153c3549ccd2b72dc58d6d7
Subho Karmakar <isubhokarma@gmail.com>
2019-04-29 19:47:20 +0530

    React app created/ Proxy: client -- server
    npx: create-react-app client
    npm: server: concurrently/ client: http-proxy-middleware -- setupProxy.js
    client\src\setupProxy.js
    auth proxy: react: client -- node+express: server

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <a href="/auth/google">Sign in with Google</a>
+    </div>
+  );
+}
+
+export default App;
