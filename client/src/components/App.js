acd409565e710f44771097b3dbed34a1edc4a92d
Subho Karmakar <isubhokarma@gmail.com>
2019-05-04 14:28:59 +0530

    componentDidMount: this.props.fetchUser/ Bootstrap: Components/ Navbar
    link/script Bootstrap: components/navbar -- working
    app comp calls fetchUser
    axios get current_user to dispatch
    dispatch action
    reduxThunk middleware applied to createStore with reducer verifying the state for action
    componentDidMount: this.props.fetchUser -- not working -- this.props.fetchUser is not a function line: 13

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
-const App = () => {
-	return (
-		<div>
-			<h3>Hello, World!</h3>
-		</div>
-	);
-};
+import Navbar from "./Navbar";
+const Dashboard = () => <h2>Dashboard</h2>;
+const New = () => <h2>New</h2>;
+const Base = () => <h2>Base</h2>;
 
-export default App;
+class App extends Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Navbar />
+						<Route exact path="/" component={Base} />
+						<Route exact path="/activity" component={Dashboard} />
+						<Route exact path="/activity/new" component={New} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+
+export default connect(
+	null,
+	actions
+)(App);
