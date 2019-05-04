efe0f1be7ba49dc6d298ae449f98f8984d434d05
Subho Karmakar <isubhokarma@gmail.com>
2019-05-05 03:26:03 +0530

    Logged In: working
    swith action: return payload or false/ default: state
    default state: null
    
    switch this.props.auth: action state: null -- return nothing/ action state: false -- return Login/ action state: default -- "next commit"

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,46 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Navbar extends Component {
+	renderContent() {
+		switch (this.props.auth) {
+			case null:
+				return;
+			case false:
+				return (
+					<a
+						className="btn btn-primary col col-lg-2 pull-right"
+						href="/auth/google"
+						role="button"
+					>
+						<i className="fab fa-google" /> Login
+					</a>
+				);
+			default:
+				return "Logged In";
+		}
+	}
+
 	render() {
 		return (
 			<nav>
 				<div className="navbar navbar-expand-lg navbar-light bg-light">
-					<a className="navbar-brand mb-0 h1 col col-lg-4 pull-left" href="#">
+					<a
+						className="navbar-brand mb-0 h1 col col-lg-4 pull-left"
+						href="localhost:3000"
+					>
 						Navbar
 					</a>
 					<div className="col" />
-					<button
-						type="button"
-						className="btn btn-outline-primary col col-lg-2 pull-right"
-					>
-						<i className="fab fa-google" /> Login
-					</button>
+					<div>{this.renderContent()}</div>
 				</div>
 			</nav>
 		);
 	}
 }
 
-export default Navbar;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Navbar);
