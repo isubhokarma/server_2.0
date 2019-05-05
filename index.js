907a4fb19b363aca4966c321033f66d664c0951c
Subho Karmakar <isubhokarma@gmail.com>
2019-05-05 12:22:14 +0530

    Routing for: production env
    It's all about the sequence of commands --
    server/Index.js:
    1. check for production env
    2. express will serve up production assets like- main.js main.cc
    3. express will serve up the index.html if it doesn't recognize the route

--- index.js
+++ index.js
@@ -25,5 +25,16 @@ app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+	//express will serve up production assets like- main.js main.cc
+	app.use(express.static("client/build"));
+
+	//express will serve up the index.html if it doesn't recognize the route
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
